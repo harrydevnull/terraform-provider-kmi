24c9740e38074ef87a172e894c58d882f23ad225
Hareendran Chandrasekharan Nair <hachandr@akamai.com>
2024-01-16 22:17:30 -0500

    added all the files

--- internal/kmi/types_test.go
+++ internal/kmi/types_test.go
@@ -2,7 +2,6 @@ package kmi
 
 import (
 	"encoding/xml"
-	"fmt"
 	"os"
 	"strings"
 
@@ -44,44 +43,6 @@ func Test_AccountMarshalling(t *testing.T) {
 
 }
 
-func Test_Account2Marshalling(t *testing.T) {
-	data, err := os.ReadFile("account_test2.xml")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	var e1 Account
-	err = xml.Unmarshal(data, &e1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err != nil {
-		t.Errorf("Marshalling() error = %v", err)
-		return
-	}
-
-	if !reflect.DeepEqual(e1.AdminGroup, "PIM_TEST_admins") {
-		t.Errorf("Marshalling() = %v, want %v", e1.AdminGroup, "PIM_TEST_admins")
-	}
-	if !reflect.DeepEqual(e1.Contact, "sme:2756") {
-		t.Errorf("Marshalling() = %v, want %v", e1.AdminGroup, "sme:2756")
-	}
-	if !reflect.DeepEqual(e1.Contact, "sme:2756") {
-		t.Errorf("Marshalling() = %v, want %v", e1.AdminGroup, "sme:2756")
-	}
-
-	if !reflect.DeepEqual(e1.Engine[len(e1.Engine)-1].Cloud, "linode") {
-		t.Errorf("Marshalling() = %v, want %v", e1.Engine[len(e1.Engine)-1].Cloud, "linode")
-	}
-	var names []string
-	for _, grp := range e1.Group {
-		//get grp name
-		names = append(names, fmt.Sprintf("\"%s\"", grp.Name))
-	}
-	fmt.Println(names)
-
-}
 func Test_EngineMarshalling(t *testing.T) {
 	data := []byte(`<workload projection="instance_validator" source="restserv:user:hachandr_kmi_cert">
 	<region source="restserv:user:hachandr_kmi_cert">us-iad</region>
@@ -120,8 +81,8 @@ func Test_EngineMarshalling(t *testing.T) {
 }
 func Test_IdentityEngineMarshalling(t *testing.T) {
 	data := []byte(`<engine name="pi-qa-automation-webapp-spa" cloud="linode" type="kubernetes" adders="PIM_TEST_admins" modifiers="PIM_TEST_admins" modified="355711849" source="restserv:user:hachandr_kmi_cert" published="2024-01-16 03:43:40" published_location="/secret/Col=kmi_identity_engines/Def=PIM_TEST.pi-qa-automation-webapp-spa/Idx=355711849">
-	<option name="cas_base64" source="restserv:user:hachandr_kmi_cert">LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUMvakNDQWVhZ0F3SUJBZ0lCQURBTkJna3Foa2lHOXcwQkFRc0ZBREFWTVJNd0VRWURWUVFERXdwcmRXSmwKY201bGRHVnpNQjRYRFRJek1EUXhOakEzTlRrMU1sb1hEVE16TURReE16QTNOVGsxTWxvd0ZURVRNQkVHQTFVRQpBeE1LYTNWaVpYSnVaWFJsY3pDQ0FTSXdEUVlKS29aSWh2Y05BUUVCQlFBRGdnRVBBRENDQVFvQ2dnRUJBS1pyCjlzbXZJbkNYbWdFSGpDRHpnRmVUMjllQ3d6a0NieVVGTzhKeDdCSXdxWElrS3YxWXFhcVA4a0k0a05yYWp6cVAKWWN0NytrdklCeHhZQTVDd1Z1VkdYVUV5Q0tESTY5d0gzdWJ5c3FLdjlHdGZoV2tjMlJwSktQYkQvRC8wRVF4RQp0TUxZY05hZC9pNit3TStrMVdSTVdPaVhyRmRldUR3ZS9HZTU5Mlc4R1NVOVNvcnIyRStXOFllN284RTRUNGxFCjFIVE80SHA2dUhUNi9DUkp2SFdNUm1aUTlDSmR5R0hDa1N1SUxvTGE1SnpDNUdjQzRIUnZCbktVNGhqVlVyZkwKd3V6b3A2TXA1UVZvRU96YVJ4ejE1eUFFekpyRkpnZG4vNmtZbTBrbkFSdTJiVXU2WkN6ZmxEdjdJVUIxU3BYWApLaXlVRmdFRTBhZUFOa3UzYWhFQ0F3RUFBYU5aTUZjd0RnWURWUjBQQVFIL0JBUURBZ0trTUE4R0ExVWRFd0VCCi93UUZNQU1CQWY4d0hRWURWUjBPQkJZRUZMREZSeDNXajBpRTZ6djhLdnpqRHJ6dnBIc3NNQlVHQTFVZEVRUU8KTUF5Q0NtdDFZbVZ5Ym1WMFpYTXdEUVlKS29aSWh2Y05BUUVMQlFBRGdnRUJBQ1lML3VUNlVwWmRFYzI1N25GLwpocWZLdVhva256NVo0bXdwM0lYQjBBQkZOdE1YNFF4MExCeEtCQnJiay8vYkRlM1ZWSEJldEVuQm1CRDBQZUdXCnFsc3p5eWtXNENrWTlmZjMvbThwazNxVmRtc0pvZFJwd20xV3NCSE1MbW8xWnlKbHN0YjcydDhEUG4vdlBMQmkKR2lZa1BvVTN2SVJoQWYrUjRGc3BVdmtZSFIzS2tOTWp4RmFaUjRqK1VnNTlsK3NXTzBGUFkwR0duMDRjblovKwowbytKcDBtNGhDMG5EY0lkWnZuTFVwUDBYZm1UNjMvc1Rvd29qdUxhSlJLVFFTa3VicEpvSmlIR2lMYzJYOVBKCnZ3bU9ma1FpdUFjWHdZQXh2OVJoU3VJV09LVU9PT0hoRTlMdWVwc2RtVXZtdHJDcXljektZSEwwMFVxNG5EUDEKWEdVPQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tCg==</option>
-	<option name="endpoint_url" source="restserv:user:hachandr_kmi_cert">https://4a7de7c1-0276-40cc-bd61-c50a89a9c553.cpc2-us-central.linodelke.net:443</option>
+	<option name="cas_base64" source="restserv:user:hachandr_kmi_cert"></option>
+	<option name="endpoint_url" source="restserv:user:hachandr_kmi_cert"></option>
 	<workload projection="instance_validator"/>
 	<workload projection="instance_validator-1"/>
   </engine>`)
@@ -149,7 +110,7 @@ func Test_IdentityEngineMarshalling(t *testing.T) {
 }
 
 func Test_IdentityEngineMarshallingSecond(t *testing.T) {
-	data := []byte(`<engine name="pi-dev-usiad-l1-2023-1127-121839" cloud="linode" type="kubernetes" adders="PIM_TEST_admins" modifiers="PIM_TEST_admins" modified="355322586" source="restserv:user:hachandr_kmi_cert" published="2024-01-13 17:30:12" published_location="/secret/Col=kmi_identity_engines/Def=PIM_TEST.pi-dev-usiad-l1-2023-1127-121839/Idx=355322586"><option name="cas_base64" source="restserv:user:hachandr_kmi_cert">LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUMvakNDQWVhZ0F3SUJBZ0lCQURBTkJna3Foa2lHOXcwQkFRc0ZBREFWTVJNd0VRWURWUVFERXdwcmRXSmwKY201bGRHVnpNQjRYRFRJek1URXlOekV5TWpBMU1sb1hEVE16TVRFeU5ERXlNakExTWxvd0ZURVRNQkVHQTFVRQpBeE1LYTNWaVpYSnVaWFJsY3pDQ0FTSXdEUVlKS29aSWh2Y05BUUVCQlFBRGdnRVBBRENDQVFvQ2dnRUJBS2ZLCjhmcjhqYjlsdFlRVittdkc4K1RSVFZiQ1VWb1NZZlJ4OVg4VGY2NVE1QzdYTFhZWTNTUjhacm5QZG9mdSttdEoKajZ2YmplQ1prOVh0K2ZZR1hhOFdKZHYxbjQ2YzUybTIzejJnQTNLQTUxaGRVWEUxQ0NzUlF6eVVldzA5K0grQwpRQjMyeVZZOVo5SjZRQ3RGVG9makN1OHUrRHllM0UrMWlyYy9UcmRlbGNCRm9xS255R2VsMi9XZWJ6UnIyVmhuCmpJUklRSEpKWCtOVE1GcEo5SFVKMUQyQTltMHZBazRmNkFIRHZKWkcxVUZkaHArbUFGajBsYVRsRTZTTytEMWMKTmFZVGZnSmtpcVIraWhKMEtCdzNWb3FSL1hqb20wMXZPcEkzZWRObFdqUjNIWXJmbnRFN1VjQTFaR2g5Q1pVOQpKZThoWmFDUThiUDJHNzRBY2prQ0F3RUFBYU5aTUZjd0RnWURWUjBQQVFIL0JBUURBZ0trTUE4R0ExVWRFd0VCCi93UUZNQU1CQWY4d0hRWURWUjBPQkJZRUZPelRKN3FOdTBMY2RnZXFpSTlWZlhQaExyK0dNQlVHQTFVZEVRUU8KTUF5Q0NtdDFZbVZ5Ym1WMFpYTXdEUVlKS29aSWh2Y05BUUVMQlFBRGdnRUJBQWpRRTdHZCtLYWhhME9lY0hlawo2dk5ZUmpUeGVaSjFETGRDc2dxZFErVVRudkF3RWRzWDBra2x5L05wWHVraWpBL011QlNYaTRMS242ZFcyalRUClIyVUpQRkswTzFoVjk3OG9USXF2QVJOV1czbStoclh3SGJVRnIrbE1MZ2s4MUgyWTBGTk5vNnlFUlAvSWtRWC8KNFpzWmNocFhVei9sU05mMm0rdVhKdUYxUVFYSDBNS3RFU1JxVnIwR3hhZmVadDNzVDE2S1ZoTTZIRThTZEk5WApadFk4VVhyYnR3ZEp0bVhjVjdmMjlZQVptcVRyVXpPa2ZjdFBrVzNVT25Ga1cwNHhkZ1Y2d2krZjlWallRbEFRCmk5dTEwMWJITjB1aEo5emVWb3JlSDdmblIwRHVPSHBqUndlcXdIK0xrZ2FBU2tjQ3ExMTJMdlVoUkRYa1h1dTEKV1E0PQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tCg==</option><option name="endpoint_url" source="restserv:user:hachandr_kmi_cert">https://fbfcd97b-cf7b-4ce6-8736-1abbd7e072b1.us-iad-1.linodelke.net:443</option><workload projection="instance_validator"/></engine>`)
+	data := []byte(`<engine name="pi-dev-usiad-l1-2023-1127-121839" cloud="linode" type="kubernetes" adders="PIM_TEST_admins" modifiers="PIM_TEST_admins" modified="355322586" source="restserv:user:hachandr_kmi_cert" published="2024-01-13 17:30:12" published_location="/secret/Col=kmi_identity_engines/Def=PIM_TEST.pi-dev-usiad-l1-2023-1127-121839/Idx=355322586"><option name="cas_base64" source="restserv:user:hachandr_kmi_cert"></option><option name="endpoint_url" source="restserv:user:hachandr_kmi_cert"></option><workload projection="instance_validator"/></engine>`)
 	var e1 IdentityEngine
 	err := xml.Unmarshal(data, &e1)
 	if err != nil {
