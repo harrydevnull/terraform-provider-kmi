c9f750118c795bf211a54f3125e0910e553d0fac
Hareendran Chandrasekharan Nair <hachandr@akamai.com>
2024-02-12 08:09:20 -0500

    added hosts and read me

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,5 +1,5 @@
 provider "kmi" {
-  host      = "https://kdc-api.shared.qa.akamai.com:11838"
+  host      = "kdc host"
   api_key   = "<path to api_cert.key>"
   api_crt   = "<path to api_cert.crt>"
   akamai_ca = "<path to akamai_ca_list.pem>"
