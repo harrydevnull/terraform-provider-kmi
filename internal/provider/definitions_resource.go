80f863c4b27689e157df1d55514f998879c5024d
Illia Priadko <ipriadko@akamai.com>
2024-07-24 13:51:43 +0200

    Revert "move mutex"
    This reverts commit 5fb1389c4129bf391cd85dcfe10ac653d2ff9de4.

--- internal/provider/definitions_resource.go
+++ internal/provider/definitions_resource.go
@@ -31,8 +31,6 @@ func NewDefinitionsResource() resource.Resource {
 // definitionsResource is the resource implementation.
 type definitionsResource struct {
 	client *kmi.KMIRestClient
-	// Can be removed once KMI API bug failing parallel requests is resolved (KMISUP-1541)
-	sync.Mutex
 }
 
 // Metadata returns the resource type name.
@@ -188,10 +186,13 @@ func (r *definitionsResource) Schema(_ context.Context, _ resource.SchemaRequest
 	}
 }
 
+// Can be removed once KMI API bug failing parallel requests is resolved (KMISUP-1541)
+var mu sync.Mutex
+
 // Create creates the resource and sets the initial Terraform state.
 func (r *definitionsResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	r.Lock()
-	defer r.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 	var plan definitionResourceModel
 	diags := req.Plan.Get(ctx, &plan)
 	resp.Diagnostics.Append(diags...)
@@ -390,8 +391,8 @@ func (r *definitionsResource) Read(ctx context.Context, req resource.ReadRequest
 
 // Update updates the resource and sets the updated Terraform state on success.
 func (r *definitionsResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	r.Lock()
-	defer r.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 	var plan definitionResourceModel
 	diags := req.Plan.Get(ctx, &plan)
 	resp.Diagnostics.Append(diags...)
@@ -522,8 +523,8 @@ func (r *definitionsResource) Update(ctx context.Context, req resource.UpdateReq
 
 // Delete deletes the resource and removes the Terraform state on success.
 func (r *definitionsResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	r.Lock()
-	defer r.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 	var state definitionResourceModel
 	diags := req.State.Get(ctx, &state)
 	resp.Diagnostics.Append(diags...)
